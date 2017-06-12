dfb1162ad16d9fc83182eeea339f53c1dce7d073
Rune Myrhaug <rune.myrhaug@uninett.no>
2017-06-12 21:55:46 +0200

    push

---
+++ powershell/checkO365UserExistence.ps1
@@ -0,0 +1,13 @@
+﻿ #Connect-MsolService
+ 
+ $lines = Get-Content c:\tmp\postfixbrukere.txt
+    $lines |
+     ForEach-Object{
+         $user = Get-MsolUser -UserPrincipalName $_ -ErrorAction SilentlyContinue
+
+         if($user) {
+         }
+         else {
+            Write-Output $_
+         }
+     }
